149beb78bc1ef9f755d765f30447b93f1b70edf5
BuildTools <unconfigured@null.spigotmc.org>
2022-12-12 23:23:46 +0400

    Initial commit

---
+++ src/main/java/net/laizer/moodyray/MoodyRay.java
@@ -0,0 +1,14 @@
+package net.laizer.moodyray;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoodyRay implements ModInitializer {
+	public static final String MOD_ID = "moodyray";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/laizer/moodyray/MoodyRayClient.java
@@ -0,0 +1,10 @@
+package net.laizer.moodyray;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoodyRayClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
